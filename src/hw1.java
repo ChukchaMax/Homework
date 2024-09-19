b3c47729e5c98581c10e952461ee5d97766c8809
ChukchaMax <3yadykin@gmail.com>
2024-09-19 08:28:09 +0300

    The first homework

--- src/hw1.java
+++ src/hw1.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class hw1 {
+    public hw1() {
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int n = in.nextInt();
+
+        String b;
+        for(b = ""; n > 0; n /= 2) {
+            b = b + n % 2;
+        }
+
+        StringBuilder sb = new StringBuilder(b);
+        sb.reverse();
+        System.out.println(sb);
+        in.close();
+    }
 }
